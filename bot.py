eb32bb71fee456085b84758ffbc136a39c91ab35
Kyoliviia <Cirlanolivia23@gmail.com>
2026-05-11 18:49:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import logging
-import nest_asyncio
 import asyncio
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
